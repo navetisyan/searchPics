9b19c9d966a48d0629afd135bbb773cf5a5d506a
Narine Avetisyan <avetisyannarine@gmail.com>
2021-03-02 01:38:23 +0400

    video search with styleing. select and play a a video

--- src/components/videosElement/videoItem.js
+++ src/components/videosElement/videoItem.js
@@ -1,22 +1,15 @@
-import React from "react";
 import "../../styles/videoItem.css";
 
-class VideoItem extends React.Component {
-  render() {
-    const { thumbnails, title, description } = this.props.video.snippet;
-    return (
-      <div className="video-item item">
-        <img
-          className="ui image"
-          alt={description}
-          src={thumbnails.medium.url}
-        />
-        <div className="content">
-          <div className="header">{title}</div>
-        </div>
+const VideoItem = ({ video, onVideoSelect }) => {
+  const { thumbnails, title, description } = video.snippet;
+  return (
+    <div className="video-item item" onClick={() => onVideoSelect(video)}>
+      <img className="ui image" alt={description} src={thumbnails.medium.url} />
+      <div className="content">
+        <div className="header">{title}</div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default VideoItem;
--- src/components/videosElement/videoPane.js
+++ src/components/videosElement/videoPane.js
@@ -2,28 +2,32 @@ import React from "react";
 import youtube from "../../api/youtube";
 import SearchBar from "../searchBar";
 import VideoList from "./videoList";
+import VideoDetail from "./videoDetail";
 
 class VideoPane extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { videos: [] };
+  state = {
+    videos: [],
+    selectedVideo: null,
+  };
 
-    this.tabRef = React.createRef();
+  componentDidMount() {
+    this.onSearchSubmit("Armenian culture");
   }
 
   onSearchSubmit = async (term) => {
-    console.log("term==", term);
-
     let result = await youtube.get("./search", {
-      snippet: term,
+      params: {
+        q: term,
+      },
     });
+    this.setState({
+      videos: result.data.items,
+      selectedVideo: result.data.items[0],
+    });
+  };
 
-    // let result = await youtube.get("./search", {
-    //   params: {
-    //     q: term,
-    //   }
-    // });
-    this.setState({ videos: result.data.items });
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
   };
 
   render() {
@@ -31,7 +35,19 @@ class VideoPane extends React.Component {
       <div>
         <SearchBar content="video" onSubmit={this.onSearchSubmit} />
         Found: {this.state.videos.length} videos
-        <VideoList videos={this.state.videos} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                videos={this.state.videos}
+                onVideoSelect={this.onVideoSelect}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
